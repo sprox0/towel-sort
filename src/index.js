96cde9382742880a885d9fecb85a5d580ffe69a2
mac <mac@macs-MacBook-Pro.local>
2023-02-28 00:10:14 +0300

    feat: ended towel sort task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,24 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  const arr = [];
+		if (matrix==null) {
+      return [];
+    }
+
+		for (i = 0; i < matrix.length; i++) {
+			if (i%2 == 0) {
+				let j = matrix[i].length;
+
+				for (j = 0; j < matrix[i].length; j++) {
+    				arr.push(matrix[i][j]);	
+  				}
+			}
+			else {
+				for (j =  matrix[i].length-1; 0 <=j; j--) {
+    			arr.push(matrix[i][j]);
+  				}
+  			}
+		}
+		return arr;
 }
