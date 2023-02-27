268f8443aeee8b01e048f1819a288855c37bb484
mac <mac@macs-MacBook-Pro.local>
2023-02-28 01:58:29 +0300

    feat: fixed code

--- src/index.js
+++ src/index.js
@@ -3,23 +3,21 @@
 
 module.exports = function towelSort (matrix) {
   const arr = [];
-		if (matrix==null) {
+		if (matrix == null) {		
       return [];
     }
 
-		for (i = 0; i < matrix.length; i++) {
-			if (i%2 == 0) {
-				let j = matrix[i].length;
-
-				for (j = 0; j < matrix[i].length; j++) {
-    				arr.push(matrix[i][j]);	
-  				}
+	for (i = 0; i < matrix.length; i++) {
+		if (i % 2 === 0) {
+			let innerArr = matrix[i].length;
+			for (innerArr = 0; innerArr < matrix[i].length; innerArr++) {
+				arr.push(matrix[i][innerArr]);	
 			}
-			else {
-				for (j =  matrix[i].length-1; 0 <=j; j--) {
-    			arr.push(matrix[i][j]);
-  				}
-  			}
-		}
+		} else {
+			for (innerArr = matrix[i].length-1; 0 <= innerArr; innerArr--) {
+			arr.push(matrix[i][innerArr]);
+			}
+		}	
+	}
 		return arr;
 }
